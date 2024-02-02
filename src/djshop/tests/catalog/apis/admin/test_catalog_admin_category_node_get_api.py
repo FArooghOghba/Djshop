6ef074dbd1630e93a3254529a34b3ad66beb374d
FArooghOghba <faroogh.oghba@gmail.com>
2024-02-02 15:48:29 +0330

    build: tests for category node admin get api

--- src/djshop/tests/catalog/apis/admin/test_catalog_admin_category_node_get_api.py
+++ src/djshop/tests/catalog/apis/admin/test_catalog_admin_category_node_get_api.py
@@ -4,7 +4,7 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
-from src.djshop.catalog.selectors.category import get_category_detail
+from src.djshop.catalog.selectors.category import get_category_node
 
 
 if TYPE_CHECKING:
@@ -16,32 +16,34 @@ if TYPE_CHECKING:
 pytestmark = pytest.mark.django_db
 
 
-def category_detail_url(category_slug: str) -> str:
+def category_admin_node_url(category_slug: str) -> str:
 
     """
-    Generate the URL for the category detail API endpoint based on the category slug.
+    Generate the URL for the category admin node API endpoint based on
+    the category slug.
 
     This function takes a category slug as input and generates the URL for the
-    movie detail API endpoint by using the `reverse` function provided by Django's
-    URL resolver. The category slug is included as a parameter in the URL.
+    category admin node API endpoint by using the `reverse` function provided
+    by Django's URL resolver. The category slug is included as a parameter in
+    the URL.
 
     :param category_slug: The slug of the category.
-    :return: The URL for the category 'detail API' endpoint.
+    :return: The URL for the category admin 'node API' endpoint.
     """
 
-    return reverse(viewname='api:catalog:category-detail', args=[category_slug])
+    return reverse(viewname='api:catalog:admin-category-node', args=[category_slug])
 
 
-def test_get_category_detail_return_success(
+def test_get_admin_category_node_get_api_return_success(
     api_client: 'APIClient', first_test_root_category: 'Category',
 ) -> None:
 
     """
-    Test that retrieving movie details for a user should succeed.
+    Test that retrieving category admin node for a user should return succeed.
 
-    This test ensures that a user can retrieve the details of a category
+    This test ensures that a user can retrieve the details of a category admin
     through the API. 'api_client.get()' method is used to perform the GET
-    request for movie details.
+    request for category details.
 
     :param first_test_root_category: A fixture providing the first test root
                                     category object.
@@ -51,23 +53,22 @@ def test_get_category_detail_return_success(
 
     first_test_category_slug = first_test_root_category.slug
 
-    url = category_detail_url(category_slug=first_test_category_slug)
+    url = category_admin_node_url(category_slug=first_test_category_slug)
 
     response = api_client.get(path=url)
     assert response.status_code == status.HTTP_200_OK
 
-    # Assertion: Check that the average rating is calculated correctly for the movie
-    test_category = get_category_detail(category_slug=first_test_category_slug)
+    test_category = get_category_node(category_slug=first_test_category_slug)
     test_category_title = test_category.title
     assert response.data['title'] == test_category_title
 
 
-def test_get_nonexistent_category_detail_return_error(
+def test_get_admin_nonexistent_category_node_get_api_return_error(
     api_client: 'APIClient'
 ) -> None:
 
     """
-    Test retrieving details of a nonexistent category.
+    Test retrieving details of a nonexistent category for admin user.
 
     This test verifies that when requesting the details of a category
     that does not exist, the API returns a not found error.
@@ -77,7 +78,7 @@ def test_get_nonexistent_category_detail_return_error(
     :return: None
     """
 
-    url = category_detail_url(category_slug='nonexistent-slug')
+    url = category_admin_node_url(category_slug='nonexistent-slug')
     response = api_client.get(path=url)
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
